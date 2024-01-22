a053ce0c4f8be474fd8c8ae98e993bbc3cea24b8
Nikolai Cherenkov <nikolai8cherenkov@gmail.com>
2024-01-22 23:42:35 +0400

    feat: implement 2 task functionality

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('node:path');
+const readline = require('node:readline');
+const filePath = path.join(__dirname, "output.txt");
+
+const fileStream = fs.createWriteStream(filePath, { flags: "a" });
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log('Enter text to write to file. Type "exit" to quit.');
+
+const handleInput = (input) => {
+    if(input.trim() === "exit") {
+        console.log("Goodbye");
+        rl.close();
+    } else {
+        fileStream.write(input + '\n');
+    }
+};
+
+rl.on("line", handleInput);
+
+process.on("SIGINT", () => {
+    console.log("\nGoodbye!");
+    rl.close();
+});
