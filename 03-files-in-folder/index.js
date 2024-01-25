4ce1bc0d31a23eb9bb7e13be31db08f64697d0f0
Nikolai Cherenkov <nikolai8cherenkov@gmail.com>
2024-01-25 13:43:05 +0400

    feat: add function to list files in directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,2 +1,29 @@
 const fs = require('fs/promises');
 const path = require('path');
+
+async function listFilesInDirectory(dirPath) {
+    try {
+        const dirents = await fs.readdir(dirPath, { withFileTypes: true });
+
+        const fileNames = [];
+
+        for (const dirent of dirents) {
+            if (dirent.isFile()) {
+                fileNames.push(dirent.name);
+            }
+        }
+        return fileNames;
+    } catch (error) {
+        console.error("Error reading directory", error);
+    }
+}
+
+// const testDirPath = path.join(__dirname, 'secret-folder');
+
+// listFilesInDirectory(testDirPath)
+//   .then(fileNames => {
+//     console.log('Files in directory:', fileNames);
+//   })
+//   .catch(error => {
+//     console.error('An error occurred:', error);
+//   });
